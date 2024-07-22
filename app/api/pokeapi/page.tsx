6fbf53c05e0c0c8e0ed5cb64d27015bee0aef0cd
Anna Baker <62150513+abaker93@users.noreply.github.com>
2024-07-22 14:44:16 -0400

    comment out pokeapi page

--- app/api/pokeapi/page.tsx
+++ app/api/pokeapi/page.tsx
@@ -1,70 +1,67 @@
-'use client'
+// import Pokedex from 'pokedex-promise-v2';
+// const P = new Pokedex();
 
-import { useEffect, useState } from 'react';
-import Pokedex from 'pokedex-promise-v2';
-const P = new Pokedex();
+// interface PokemonProperties {
+// 	name: string,
+// 	names: Names[]
+// 	order: number,
+// 	variations: Variation[]
+// }
 
-interface PokemonProperties {
-	name: string,
-	names: Names[]
-	order: number,
-	variations: Variation[]
-}
+// interface Names {
+// 	lang: string,
+// 	name: string
+// }
 
-interface Names {
-	lang: string,
-	name: string
-}
+// interface Variation {
+// 	default: boolean,
+// 	order: number,
+// 	sprites: Sprite
+// }
 
-interface Variation {
-	default: boolean,
-	order: number,
-	sprites: Sprite
-}
+// interface Sprite {
+// 	default: string,
+// 	shiny: string
+// }
 
-interface Sprite {
-	default: string,
-	shiny: string
-}
+// export default function displayPokeAPI() {
+	// const [pokemon, setPokemon] = useState<PokemonProperties[]>([])
 
-export default function displayPokeAPI() {
-	const [pokemon, setPokemon] = useState<PokemonProperties[]>([])
+	// const getPokemon = (entries:any) => {
+	// 	entries.forEach((ent:any) => {
+	// 		const entryNum = ent.entry_number
 
-	const getPokemon = (entries:any) => {
-		entries.forEach((ent:any) => {
-			const entryNum = ent.entry_number
+	// 		P.getResource(ent.pokemon_species.url)
+	// 			.then(res => {
+	// 				setPokemon(currentList => [...currentList, {
+	// 					name: res.name,
+	// 					names: res.names,
+	// 					order: entryNum,
+	// 					variations: [
+	// 						{
+	// 							default: true,
+	// 							order: 0,
+	// 							sprites: {
+	// 								default: "https://img.pokemondb.net/sprites/home/normal/"+res.name+".png",
+	// 								shiny: "https://img.pokemondb.net/sprites/home/shiny/"+res.name+".png"
+	// 							}
+	// 						}
+	// 					]
+	// 				}])
+	// 			})
+	// 	})
+	// }
 
-			P.getResource(ent.pokemon_species.url)
-				.then(res => {
-					setPokemon(currentList => [...currentList, {
-						name: res.name,
-						names: res.names,
-						order: entryNum,
-						variations: [
-							{
-								default: true,
-								order: 0,
-								sprites: {
-									default: "https://img.pokemondb.net/sprites/home/normal/"+res.name+".png",
-									shiny: "https://img.pokemondb.net/sprites/home/shiny/"+res.name+".png"
-								}
-							}
-						]
-					}])
-				})
-		})
-	}
+	// const getPokedex = (dex:string) => {
+	// 	P.getPokedexByName(dex)
+	// 		.then(res => {
+	// 			getPokemon(res.pokemon_entries)
+	// 		})
+	// }
 
-	const getPokedex = (dex:string) => {
-		P.getPokedexByName(dex)
-			.then(res => {
-				getPokemon(res.pokemon_entries)
-			})
-	}
+	// useEffect(() => {
+	// 	getPokedex('paldea')
+	// }, [])
 
-	useEffect(() => {
-		getPokedex('paldea')
-	}, [])
-
-	return JSON.stringify(pokemon)
-}
+	// return JSON.stringify(pokemon)
+// }
